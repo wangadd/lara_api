8407a4325d5ab0bab7b1b710833780654e213951
wangadd <1276321779@qq.com>
2019-03-14 08:59:43 +0000

    first commit

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redis;
+
+
+class UserController extends Controller
+{
+    protected $redis_key="user_token";
+    public function test(){
+        echo __METHOD__;
+    }
+
+    public function userLogin(Request $request){
+        $username=$request->input('u');
+        $pass=$request->input('p');
+        //验证用户信息
+
+        $res=true;
+        if($res){
+            $uid=100;
+            $str=time().$uid.rand(1111,9999);
+            $token=substr(md5($str),10,20);
+
+            //将token 存入redis
+            $key=$this->redis_key.':'.$uid;
+            Redis::hSet($key,'token',$token);
+            Redis::expire($key,3600*24*7);
+            echo $token;
+        }
+    }
+}
+
